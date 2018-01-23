0baa4218166fe313cd1609d28f9a407fdc53e7a4
Jaremy Creechley <creechley@gmail.com>
2018-01-22 17:19:49 -0700

    experiment... heh

--- lib/shell_template.ex
+++ lib/shell_template.ex
@@ -3,16 +3,56 @@ defmodule ShellTemplate do
   Documentation for ShellTemplate.
   """
 
+  @bracket_regex ~r/(?!\\) \${ (\w+) }/x
+  @single_regex ~r/(?!\\) \$ (\w+) /x
+
   @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> ShellTemplate.hello
-      :world
+      iex> ShellTemplate.format("test ${TEST}!", %{TEST: "hello world"})
+      "hello world!"
 
   """
-  def hello do
-    :world
+  def format(template, values) when is_map(values) do
+    result = ""
+    IO.puts "results: #{inspect result}"
+    ""
   end
+
 end
+
+defmodule TestGrammar do
+  use Neotomex.ExGrammar
+
+  @root true
+  define :top, "elem+" do
+    all ->
+      IO.puts "top: #{inspect all}"
+      all
+  end
+
+  define :elem, "space? (var / number) space?" do
+    all ->
+      IO.puts "elem: #{inspect all}"
+      "elem:"<>inspect(all)
+  end
+
+  define :var, "dollar word (word* digit)*" do
+    all ->
+      IO.puts "var: #{inspect all}"
+      "var:"<>inspect(all)
+  end
+
+  define :number, "digit+" do
+    digits ->
+      IO.puts "number: #{inspect digits}"
+      digits |> Enum.join |> String.to_integer
+  end
+
+  define :dollar, "[$]"
+  #define :word, "[A-Za-z]"
+  define :word, "[A-Za-z]"
+  define :digit, "[0-9]"
+
+  define :space, "[ \\r\\n\\s\\t]"
+end
+
